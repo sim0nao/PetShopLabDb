4edd35819e01d5e650db6af7d242904fe07d2165
jaideaa <jaide.sp@gmail.com>
2021-11-23 22:54:21 -0300

    Atualização das classes modelo e código da classe Conexão.

--- src/model/Animal.java
+++ src/model/Animal.java
@@ -1,5 +1,62 @@
 package model;
 
 public class Animal {
-
+	
+	private int id;
+	private String nome;
+	private String raca;
+	private String porte;
+	private String especie;
+	private String sexo;
+	
+	/* ---------------------------------------------------------------- */
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getRaca() {
+		return raca;
+	}
+	
+	public void setRaca(String raca) {
+		this.raca = raca;
+	}
+	
+	public String getPorte() {
+		return porte;
+	}
+	
+	public void setPorte(String porte) {
+		this.porte = porte;
+	}
+	
+	public String getEspecie() {
+		return especie;
+	}
+	
+	public void setEspecie(String especie) {
+		this.especie = especie;
+	}
+	
+	public String getSexo() {
+		return sexo;
+	}
+	
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+	
 }
