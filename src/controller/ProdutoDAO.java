1ff39ec55f82662f6379d3a3649c12385164fde1
jaideaa <54504597+jaideaa@users.noreply.github.com>
2021-11-24 00:30:30 -0300

    Atualização da classe ProdutoDAO

--- src/controller/ProdutoDAO.java
+++ src/controller/ProdutoDAO.java
@@ -1,7 +1,15 @@
 package controller;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.swing.JOptionPane;
+
 import model.Produto;
 
 
@@ -15,7 +23,7 @@ public class ProdutoDAO {
 	/* ---------------------------------------------------------------- */
 	
 	public static boolean update() {
-		// TODO: código aqui
+		// TODO: código com STORED PROCEDURES aqui		
 		return true;
 	}
 	
@@ -23,8 +31,30 @@ public class ProdutoDAO {
 	
 	public static List<Produto> list() {
 		List<Produto> lista = new ArrayList<>();
+		String sql = "SELECT * FROM Produtos";
+		Connection conn = null;
 		
-		// TODO: código aqui
+		try {
+			conn = Conexao.abrir("");
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery(sql);
+			
+			while (rs.next()) {
+				Produto p = new Produto();
+				p.setId(rs.getInt("id"));
+				p.setNome(rs.getString("nome"));
+				p.setDescricao(rs.getString("descricao"));
+				p.setValor(rs.getDouble("valor"));
+				lista.add(p);
+			}
+			
+			rs.close();
+			stmt.close();
+			conn.close();
+		} catch(SQLException e) {
+			JOptionPane.showMessageDialog(null, e.getMessage(), "Sql Exception", JOptionPane.ERROR_MESSAGE);
+			lista = null;
+		}
 		
 		return lista;
 	}
@@ -33,8 +63,26 @@ public class ProdutoDAO {
 	
 	public static Produto consultaProduto(int id) {
 		Produto p = new Produto();
+		String sql = "SELECT * FROM Produtos WHERE id = ?";
 		
-		// TODO: código aqui
+		try {
+			Connection conn = Conexao.abrir("");
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setInt(1, id);
+			ResultSet rs = ps.executeQuery();
+			
+			rs.next();	
+			p.setId(rs.getInt("id"));
+			p.setNome(rs.getString("nome"));
+			p.setDescricao(rs.getString("descricao"));
+			p.setValor(rs.getDouble("valor"));
+			
+			rs.close();
+			ps.close();
+			conn.close();
+		} catch(SQLException e) {
+			JOptionPane.showMessageDialog(null, e.getMessage(), "Sql Exception", JOptionPane.ERROR_MESSAGE);
+		}
 		
 		return p;
 	}
